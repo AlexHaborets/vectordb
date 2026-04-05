9174d3de7d49b7110907fbfa94a46428f87d99f2
Alex <121190111+pragmaticcs@users.noreply.github.com>
2026-04-05 18:25:36 +0300

    started work on the PI alpha controller

--- src/engine/indexers/vamana/ops.py
+++ src/engine/indexers/vamana/ops.py
@@ -24,7 +24,7 @@ def compute_dist_cosine(a: np.ndarray, b: np.ndarray) -> float:
     sum_value = 0.0
     for i in range(a.shape[0]):
         sum_value += a[i] * b[i]
-    return max(0.0, 1 - sum_value)
+    return 1 - sum_value
 
 
 @nb.njit(inline="always", **NUMBA_OPTIONS)
